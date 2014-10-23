73d1282d33059283666fc521ad32401cc5049ca5
mariaa7 <mariaa7@uw.edu>
2014-10-23 10:13:18 -0700

    Made changes to index HTML and Signup JS

--- js/signup.js
+++ js/signup.js
@@ -2,3 +2,25 @@
     Signup Form Script
     This script will load the state select list and validate the form before submission
 */
+
+"use strict";
+
+function Function() {
+	var elem = document.getElementById("state");
+	for (var i = 0; i < usStates.length; i++) {
+		var option = document.createElement("option");
+		option.value = usStates[i].code;
+		var t = document.createTextNode(usStates[i].name);
+		option.appendChild(t);
+		elem.appendChild(option);
+	}
+
+	//var occup = document.getElementById("occupation");
+	//occup.addEventListener('change', selectOccup);
+}
+
+/*function selectOccup(evt) {
+	var elem = document.getElementById("occupation");
+}*/
+
+document.addEventListener('DOMContentLoaded', Function);
