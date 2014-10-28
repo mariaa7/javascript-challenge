4e059079c785cafcb84de74bca5b2ed251ab1063
mariaa7 <mariaa7@uw.edu>
2014-10-27 21:37:26 -0700

    Made changes to Main CSS, Index HTML, and Signup JS

--- js/signup.js
+++ js/signup.js
@@ -15,12 +15,115 @@ function Function() {
 		elem.appendChild(option);
 	}
 
-	//var occup = document.getElementById("occupation");
-	//occup.addEventListener('change', selectOccup);
+	var occup = document.getElementById("occupation");
+	occup.addEventListener('change', selectOccup);
+
+	var button = document.getElementById('cancelButton');
+	button.addEventListener('click', cancel);
+
+	var form = document.getElementById('signup');
+	form.addEventListener('submit', submit);
 }
 
-/*function selectOccup(evt) {
-	var elem = document.getElementById("occupation");
-}*/
+function submit(evt) {
+	var valid = validate(this);
+
+	if (!valid && evt.preventDefault) {
+		evt.preventDefault();
+	}
+
+	evt.returnValue = valid;
+	return valid;
+}
+
+function selectOccup() {
+	var other = document.getElementById("occupation-other")
+	if (this.value == "other") {
+		other.style.display = 'block';
+	} else {
+		other.value = "";
+		other.style.display = 'none';
+	}
+}
+
+function cancel() {
+	if (window.confirm('Do you really want to leave this page?')) {
+		window.location.href = 'http://google.com/';
+	}
+}
+
+function validate(form) {
+	var requiredFields = ['firstName', 'lastName', 'address1', 'city', 'state', 'zip', 'birthdate'];
+	var valid = true;
+	for (var i = 0; i < requiredFields.length; i++) {
+		valid &= validateField(requiredFields[i], form);
+	}
+
+	var birthdate = document.getElementById('birthdate').value;
+	valid &= validateBirthdate(birthdate, form);
+
+	var zip = document.getElementById('zip').value;
+	valid &= validateZip(zip, form);
+
+	var occupation = document.getElementById('occupation').value;
+	if (occupation == "other") {
+		valid &= validateField('occupation-other', form);
+	}
+
+	return valid;
+}
+
+function validateField(field, form) {
+	if (0 == form[field].value.trim().length) {
+		form[field].className = 'form-control invalid';
+		return false;
+	} else {
+		form[field].className = 'form-control';
+		return true;
+	}
+}
+
+function validateBirthdate(birthdate, form) {
+	var currDate = new Date();
+	var currYear = currDate.getFullYear();
+	var currMonth = currDate.getMonth();
+	var currDay = currDate.getDate();
+	var pattern = /^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/;
+	var sep = birthdate.match(pattern);
+	if (sep == null) {
+		form['birthdate'].className = 'form-control invalid';
+		return false;
+	}
+	birthdate = new Date(sep[3], sep[1] - 1, sep[2]);
+	var year = birthdate.getFullYear();
+	var age = currYear - year;
+	var month = birthdate.getMonth();
+	var day = birthdate.getDate();
+	if (currMonth < month || month == currMonth 
+		&& currDay < day) {
+		age--;
+	}
+	if (age < 13) {
+		form['birthdate'].className = 'form-control invalid';
+		document.getElementById('birthdateMessage').innerHTML =
+		"You must be 13 years or older to sign up!";
+		return false;
+	} else {
+		form['birthdate'].className = 'form-control';
+		document.getElementById('birthdateMessage').innerHTML = "";
+		return true;
+	}
+}
+
+function validateZip(zip, form) {
+	var zipRegExp = new RegExp('^\\d{5}$');
+	if (zipRegExp.test(zip)) {
+		form['zip'].className = 'form-control';
+		return true;
+	} else {
+		form['zip'].className = 'form-control invalid';
+		return false;
+	}
+}
 
 document.addEventListener('DOMContentLoaded', Function);
